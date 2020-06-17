30efb2fea40be94b83ae325f2f1b1dc790d27b14
sirems <siremsoydan@icloud.com>
2020-06-17 22:14:43 +0300

    Basket Migration-Basket Service

---
+++ src/ApplicationCore/Specifications/BasketWithItemsSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Entities;
+using Ardalis.Specification;
+
+namespace ApplicationCore.Specifications
+{
+    public class BasketWithItemsSpecification:BaseSpecification<Basket>
+    {
+        public BasketWithItemsSpecification(int basketId):base(x => x.Id == basketId)
+        {
+            AddInclude(x=>x.Items);
+        }
+
+        public BasketWithItemsSpecification(string buyerId):base(x=>x.BuyerId==buyerId)
+        {
+            AddInclude(x=>x.Items);
+        }
+    }
+}
---
+++ src/Infrastructure/Data/Config/BasketItemConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class BasketItemConfiguration:IEntityTypeConfiguration<BasketItem>
+    {
+        public void Configure(EntityTypeBuilder<BasketItem> builder)
+        {
+            builder.Property(x => x.UnitPrice)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
